0b5aebc2ae6441c15b9570a81a0ef017e028be48
Bilal Haroon <bilal.a.haroon@protonmail.com>
2026-04-17 07:56:50 -0500

    css df clustered

---
+++ src/model.py
@@ -0,0 +1,12 @@
+from sklearn.cluster import KMeans
+from src.pipeline import run
+
+css_df = run()
+X = css_df[["success_rate_scaled", "explosiveness_scaled", "reliability_scaled"]]
+kmeans = KMeans(n_clusters=4, random_state=42).fit(X)
+css_df["cluster"] = kmeans.labels_
+
+css_df.to_csv('outputs/css_df_clustered.csv')
+
+clustered_df = css_df.groupby(css_df['cluster']).count()
+print(clustered_df.head(20))
